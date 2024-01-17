d366bf1afaf99bb2dcbfafbee29c7e5407a2973b
UmeshEkhande <149470281+UmeshEkhande@users.noreply.github.com>
2024-01-17 18:43:06 +0530

    Adding RevitAPI Project

---
+++ AddButton/Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following set of attributes
+[assembly: AssemblyTitle("Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("757d31c3-35e3-4036-ab19-40fcccc2abd4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AddPanel/AddPanel/CsAddPanel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace AddPanel
+{
+   
+    // Entry point of the application
+
+    public class CsAddPanel : IExternalApplication
+    {
+   
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+
+            // To Create a push button
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdHelloWorld",
+               "Hello World", thisAssemblyPath, "AddPanel.HelloWorld");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            pushButton.ToolTip = "Say hello to the entire world.";
+
+            Uri uriImage = new Uri(@"C:\Users\Kunal More\Downloads\download.png");
+            BitmapImage largeImage = new BitmapImage(uriImage);
+            pushButton.LargeImage = largeImage;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }    /// The "HelloWorld" external command.
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+  
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+            ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
